fcad432b9699c1252c499aa9f1ea18d74011ce95
谢康 <xk@meitu.com>
2019-03-19 17:48:13 +0800

    日志器和setting

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
+import logging
+
+from django.conf import settings
 from django.shortcuts import render
+
+logger = logging.getLogger("blog.views")
 
 
 # Create your views here.
+
+def global_setting(request):
+    return {'SITE_NAME': settings.SITE_NAME, 'SITE_DESC': settings.SITE_DESC}
+
 
 def index(request):
     return render(request, "blog/index.html", locals())
