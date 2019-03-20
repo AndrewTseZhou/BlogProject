60c9a9238c5662a394c29d3381f374d731aa15f4
谢康 <xk@meitu.com>
2019-03-20 22:11:46 +0800

    文章归档实现

--- blog/views.py
+++ blog/views.py
@@ -29,5 +29,25 @@ def index(request):
     except (EmptyPage, InvalidPage, PageNotAnInteger):
         article_list = paginator.page(1)
 
+    # 文章归档
+    # 1. 先获取文章中的年月
+    archive_list = Article.objects.distinct_date()
+
     return render(request, "blog/index.html", locals())  # 使用locals()将当前函数的所有局部变量传递到页面中
     # return render(request, "blog/index.html", {'category_list': category_list, 'article_list': article_list})
+
+
+def archives(request):
+    try:
+        year = request.GET.get('year', None)
+        month = request.GET.get('month', None)
+        article_list = Article.objects.filter(date_publish__icontaines=year + '-' + month)
+        paginator = Paginator(article_list, 10)
+        try:
+            page = int(request.GET.get('page', 1))
+            article_list = paginator.page(page)
+        except (EmptyPage, InvalidPage, PageNotAnInteger):
+            article_list = paginator.page(1)
+    except:
+        pass
+    return render(request, 'blog/archive.html', locals())
